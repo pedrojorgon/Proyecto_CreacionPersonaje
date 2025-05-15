90c3b34b0c76bb04e9c9faa4dc6067f4754d4dab
pedrojorgon <pedro.jorgon@educa.jcyl.es>
2025-05-15 16:30:31 +0200

    Cambios en ConsultasBD.java.

--- src/main/java/Controlador/CRUD/ConsultasBD.java
+++ src/main/java/Controlador/CRUD/ConsultasBD.java
@@ -2,6 +2,7 @@ package Controlador.CRUD;
 
 import Controlador.Conexion.GestorConexion;
 import Controlador.UTIL.InfoError;
+import Modelo.Objetos.Personaje;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -17,6 +18,7 @@ public class ConsultasBD {
 
     /**
      * Consulta el Id más alto de una tabla.
+     *
      * @param tabla String Tabla a consultar.
      * @return int Id máximo.
      */
@@ -33,25 +35,65 @@ public class ConsultasBD {
                 if (res == 0) {
                     if (tabla.equals("usuarios")) {
                         error = InfoError.USUARIO_NO_ENCONTRADO;
+                        System.err.printf(InfoError.getMensaje(error));
                     } else {
                         error = InfoError.PERSONAJE_NO_ENCONTRADO;
+                        System.err.printf(InfoError.getMensaje(error));
                     }
                     return -1;
                 } else {
                     error = InfoError.OK;
+                    System.out.printf(InfoError.getMensaje(error));
                     return res;
                 }
             } else {
                 if (tabla.equals("usuarios")) {
                     error = InfoError.USUARIO_NO_ENCONTRADO;
+                    System.err.printf(InfoError.getMensaje(error));
                 } else {
                     error = InfoError.PERSONAJE_NO_ENCONTRADO;
+                    System.err.printf(InfoError.getMensaje(error));
                 }
                 return -1;
             }
         } catch (SQLException sqle) {
             error = InfoError.FALLO_CONSULTA;
+            System.err.printf(InfoError.getMensaje(error));
             return -1;
+        }
+    }
+
+    /**
+     * Devuelve un objeto Personaje de la base de datos.
+     *
+     * @param idPersonaje Id del personaje.
+     * @return Personaje o null.
+     */
+    public static Personaje leerPersonaje(int idPersonaje) {
+        Connection conn = GestorConexion.getConexion();
+        int error;
+        try {
+            String sql = "SELECT nombre, idCasco, idPecho, idPiernas FROM personajes WHERE id = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, idPersonaje);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                String nombre = rs.getString("nombre");
+                int idCasco = rs.getInt("idCasco");
+                int idPecho = rs.getInt("idPecho");
+                int idPiernas = rs.getInt("idPiernas");
+                error = InfoError.OK;
+                System.out.printf(InfoError.getMensaje(error));
+                return new Personaje(idPersonaje, nombre, idCasco, idPecho, idPiernas);
+            } else {
+                error = InfoError.PERSONAJE_NO_ENCONTRADO;
+                System.err.printf(InfoError.getMensaje(error));
+                return null;
+            }
+        } catch (SQLException sqle) {
+            error = InfoError.FALLO_CONSULTA;
+            System.err.printf(InfoError.getMensaje(error));
+            return null;
         }
     }
 }
