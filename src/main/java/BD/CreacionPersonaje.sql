c1bf06e4c2894a7d79c9953d093a40bb356aea8e
pedrojorgon <pedro.jorgon@educa.jcyl.es>
2025-05-16 09:51:20 +0200

    Se añade script de la base de datos asociada.

---
+++ src/main/java/BD/CreacionPersonaje.sql
@@ -0,0 +1,20 @@
+create database creacionPersonaje;
+use creacionPersonaje;
+
+create table usuarios (
+	id_usuario INT PRIMARY KEY, -- ID como clave primaria
+    nombre VARCHAR(16) NOT NULL UNIQUE, -- Nombre del usuario
+    password VARCHAR(16) NOT NULL -- Contraseña del usuario
+);
+
+create table personajes (
+	id_usuario INT NOT NULL, -- ID como clave foránea
+	id_personaje INT NOT NULL, -- ID del personaje
+    fecha_guardado TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, -- Fecha de guardado
+    nombre VARCHAR(16), -- Nombre del personaje
+    id_casco INT NOT NULL, -- Id imagen casco
+    id_pecho INT NOT NULL, -- Id imagen pecho
+    id_piernas INT NOT NULL, -- Id imagen piernas
+    FOREIGN KEY (id_usuario) REFERENCES USUARIOS(id_usuario) ON DELETE CASCADE, -- Clave foránea de la tabla usuarios
+    PRIMARY KEY (id_usuario, id_personaje) -- Clave primaria compuesta por usuario y personaje
+);
